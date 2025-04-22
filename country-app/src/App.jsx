bd0f205effded9d85ac0b5383658fb8c4ecb8d84
unknown <you@example.com>
2025-04-22 15:43:12 +0700

    renama folder country app make country-app

--- country-app/src/App.jsx
+++ country-app/src/App.jsx
@@ -7,9 +7,9 @@ import {
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useState, useEffect } from 'react';
-import CountryList from '../components/CountryList';
-import FavoritesPage from '../components/FavoritesPage';
-import '../App.css';
+import CountryList from '../../country-app/components/CountryList';
+import FavoritesPage from '../../country-app/components/FavoritesPage';
+import '../../country-app/App.css';
 import axios from 'axios'; 
 
 function App() {
